518bc5f75fb1c5528f4f79135d07a95d139f1b1f
Attilius <leo20@freemail.hu>
2022-08-25 22:54:59 +0200

    Added BasicVariables.js

---
+++ src/BasicVariables.js
@@ -0,0 +1,22 @@
+import blue from './images/blue.png';
+import green from './images/green.png';
+import orange from './images/orange.png';
+import purple from './images/purple.png';
+import red from './images/red.png';
+import yellow from './images/yellow.png';
+
+export const width = 8;
+export const colors = [
+    blue,
+    green,
+    orange,
+    purple,
+    red,
+    yellow
+];
+export const sumOfThree = (width * 6) - 1;
+export const sumOfFour = (width * 5) - 1;
+export const sumOfFive = (width * 4) - 1;
+export const sumOfMove = (width * width) - width;
+export const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+export const moves = 10;
