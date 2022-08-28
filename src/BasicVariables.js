89424c7841f80818e771c9b3e30a73a8a20b2411
Attilius <leo20@freemail.hu>
2022-08-28 20:46:44 +0200

    Modified BasicVariables.js

--- src/BasicVariables.js
+++ src/BasicVariables.js
@@ -4,8 +4,20 @@ import orange from './images/orange.png';
 import purple from './images/purple.png';
 import red from './images/red.png';
 import yellow from './images/yellow.png';
+import blue_striped_horisontal from './images/Blue-Striped-Horizontal.png'
+import blue_striped_vertical from './images/Blue-Striped-Vertical.png'
+import green_striped_horisontal from './images/Green-Striped-Horizontal.png'
+import green_striped_vertical from './images/Green-Striped-Vertical.png'
+import orange_striped_horisontal from './images/Orange-Striped-Horizontal.png'
+import orange_striped_vertical from './images/Orange-Striped-Vertical.png'
+import purple_striped_horisontal from './images/Purple-Striped-Horizontal.png'
+import purple_striped_vertical from './images/Purple-Striped-Vertical.png'
+import red_striped_horisontal from './images/Red-Striped-Horizontal.png'
+import red_striped_vertical from './images/Red-Striped-Vertical.png'
+import yellow_striped_horisontal from './images/Yellow-Striped-Horizontal.png'
+import yellow_striped_vertical from './images/Yellow-Striped-Vertical.png'
 
-export const width = 8;
+export const width = 7;
 export const colors = [
     blue,
     green,
@@ -14,9 +26,26 @@ export const colors = [
     red,
     yellow
 ];
+export const striped = [
+    blue_striped_horisontal,
+    blue_striped_vertical,
+    green_striped_horisontal,
+    green_striped_vertical,
+    orange_striped_horisontal,
+    orange_striped_vertical,
+    purple_striped_horisontal,
+    purple_striped_vertical,
+    red_striped_horisontal,
+    red_striped_vertical,
+    yellow_striped_horisontal,
+    yellow_striped_vertical
+];
+export const wrapped = [];
 export const sumOfThree = (width * 6) - 1;
 export const sumOfFour = (width * 5) - 1;
 export const sumOfFive = (width * 4) - 1;
 export const sumOfMove = (width * width) - width;
 export const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
-export const moves = 10;
+export const moves = 20;
+//export const firstRow = [0, 1, 2, 3, 4, 5, 6];
+//export const sumOfMove = (width * width) - width;
