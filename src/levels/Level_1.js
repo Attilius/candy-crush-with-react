80fc874075da83645d569e71782e3275c44f7bcf
Attilius <leo20@freemail.hu>
2022-08-31 19:05:59 +0200

    Modified Level_1.js

--- src/levels/Level_1.js
+++ src/levels/Level_1.js
@@ -16,11 +16,11 @@ const Level_1 = (setCurrentColorArrangement) => {
     const createBoard = () => {
         const starterColorArrangement = [
         colors[4], colors[3], colors[5], colors[0], colors[5], colors[5], colors[4],
-        '', colors[3], colors[5], colors[1], colors[0], colors[0], '',
+        '', colors[3], colors[5], colors[4], colors[0], colors[0], '',
         colors[1], colors[1], colors[0], colors[4], colors[5], colors[3], colors[5],
         colors[1], colors[5], colors[4], colors[0], colors[4], colors[0], colors[3],
-        colors[5], colors[5], colors[3], colors[1], colors[3], colors[5], colors[1],
-        '', colors[1], colors[1], colors[5], colors[5], colors[1], '',
+        colors[5], colors[5], colors[3], colors[4], colors[3], colors[5], colors[1],
+        '', colors[1], colors[1], colors[4], colors[5], colors[1], '',
         colors[4], colors[0], colors[3], colors[1], colors[3], colors[5], colors[4],
     ];
     setCurrentColorArrangement(starterColorArrangement);
