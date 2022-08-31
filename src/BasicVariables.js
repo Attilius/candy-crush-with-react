183188e8f859ae9790a4fedbf2e802bfc7fb1a5c
Attilius <leo20@freemail.hu>
2022-08-31 18:57:06 +0200

    Modified BasicVariables.js

--- src/BasicVariables.js
+++ src/BasicVariables.js
@@ -4,17 +4,17 @@ import orange from './images/orange.png';
 import purple from './images/purple.png';
 import red from './images/red.png';
 import yellow from './images/yellow.png';
-import blue_striped_horisontal from './images/Blue-Striped-Horizontal.png'
+import blue_striped_horizontal from './images/Blue-Striped-Horizontal.png'
 import blue_striped_vertical from './images/Blue-Striped-Vertical.png'
-import green_striped_horisontal from './images/Green-Striped-Horizontal.png'
+import green_striped_horizontal from './images/Green-Striped-Horizontal.png'
 import green_striped_vertical from './images/Green-Striped-Vertical.png'
-import orange_striped_horisontal from './images/Orange-Striped-Horizontal.png'
+import orange_striped_horizontal from './images/Orange-Striped-Horizontal.png'
 import orange_striped_vertical from './images/Orange-Striped-Vertical.png'
-import purple_striped_horisontal from './images/Purple-Striped-Horizontal.png'
+import purple_striped_horizontal from './images/Purple-Striped-Horizontal.png'
 import purple_striped_vertical from './images/Purple-Striped-Vertical.png'
-import red_striped_horisontal from './images/Red-Striped-Horizontal.png'
+import red_striped_horizontal from './images/Red-Striped-Horizontal.png'
 import red_striped_vertical from './images/Red-Striped-Vertical.png'
-import yellow_striped_horisontal from './images/Yellow-Striped-Horizontal.png'
+import yellow_striped_horizontal from './images/Yellow-Striped-Horizontal.png'
 import yellow_striped_vertical from './images/Yellow-Striped-Vertical.png'
 
 export const width = 7;
@@ -26,18 +26,57 @@ export const colors = [
     red,
     yellow
 ];
-export const striped = [
-    blue_striped_horisontal,
+
+export const __colors = [
+    [blue, 'blue'],
+    [green, 'green'],
+    [orange, 'orange'],
+    [purple, 'purple'],
+    [red, 'red'],
+    [yellow, 'yellow']
+];
+
+export const _colors = [
+    {
+        src: blue, 
+        alt: 'blue'
+    },
+    {
+        src: green, 
+        alt: 'green'
+    },
+    {
+        src: orange, 
+        alt: 'orange'
+    },
+    {
+        src: purple, 
+        alt: 'purple'
+    },
+    {
+        src: red, 
+        alt: 'red'
+    },
+    {
+        src: yellow, 
+        alt: 'yellow'
+    }
+];
+export const stripedHorizontal = [
+    blue_striped_horizontal,
+    green_striped_horizontal,
+    orange_striped_horizontal,
+    purple_striped_horizontal,
+    red_striped_horizontal,
+    yellow_striped_horizontal
+];
+
+export const stripedVertical = [
     blue_striped_vertical,
-    green_striped_horisontal,
     green_striped_vertical,
-    orange_striped_horisontal,
     orange_striped_vertical,
-    purple_striped_horisontal,
     purple_striped_vertical,
-    red_striped_horisontal,
     red_striped_vertical,
-    yellow_striped_horisontal,
     yellow_striped_vertical
 ];
 export const wrapped = [];
@@ -49,3 +88,16 @@ export const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
 export const moves = 20;
 //export const firstRow = [0, 1, 2, 3, 4, 5, 6];
 //export const sumOfMove = (width * width) - width;
+export const Colors = [
+    {blue: blue},
+    {green: green},
+    {orange: orange},
+    {purple: purple},
+    {red: red},
+    {yellow: yellow}
+];
+
+/*
+console.log(Colors[4].red)
+console.log(Object.keys(Colors[4])[0])
+*/
